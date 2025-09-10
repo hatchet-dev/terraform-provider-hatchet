f930276845c0deca1a2931047c4c29fd5fe0c2d0
Mohammed Nafees <hello@mnafees.me>
2025-09-10 14:43:25 +0200

    fix docs

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "hatchet" {
-  # JWT token is read from HATCHET_CLOUD_MANAGEMENT_TOKEN environment variable
+  # JWT is read from HATCHET_CLOUD_MANAGEMENT_TOKEN environment variable
   # or specify it here (not recommended for production)
   # token = "eyJhbGciOiJFUzI1NiIs..."
 }
